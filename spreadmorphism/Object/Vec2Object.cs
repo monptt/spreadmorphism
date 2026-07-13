943f33c4b284b0ab20f67efc0cfe589fd403cfa7
monptt <ptt_kr@outlook.jp>
2026-07-14 00:33:08 +0900

    他のオブジェクトを参照させてみる

--- spreadmorphism/Object/Vec2Object.cs
+++ spreadmorphism/Object/Vec2Object.cs
@@ -17,8 +17,35 @@ public partial class Vec2Object : ObjectBase
         return new List<Cell> { cell_x, cell_y };
     }
 
+    void SetValue(int x, int y)
+    {
+        cell_x.SetValue(x);
+        cell_y.SetValue(y);
+    }
+
     protected override void ParseValueStr(string valueStr)
     {
-        //@todo: 実装
+        if (valueStr == "")
+        {
+            return;
+        }
+
+        // [x,y] みたいな形式を読みたい（仮）
+        if (valueStr[0] == '[' && valueStr[valueStr.Length - 1] == ']')
+        {
+            valueStr = valueStr.Substring(1, valueStr.Length - 2);
+        }
+
+        string[] values = valueStr.Split(',');
+        if (values.Length == 2)
+        {
+            GridPos targetPos = new GridPos(int.Parse(values[0]), int.Parse(values[1]));
+
+            ObjectBase obj = ObjectSpace.Instance.GetObject(targetPos);
+            if (obj is Vec2Object targetObj)
+            {
+                this.SetValue(targetObj.cell_x.Value, targetObj.cell_y.Value);
+            }
+        }
     }
 }
